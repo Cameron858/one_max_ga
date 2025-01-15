c643ccae13643dd57c966c2b513288e5a85dfae2
Cameron Wood <cameron.wood@capgemini.com>
2025-01-15 10:52:29 +0000

    Added size property to Population class.

--- src/one_max_ga/population.py
+++ src/one_max_ga/population.py
@@ -8,6 +8,7 @@ class Population:
         if not isinstance(size, int):
             raise ValueError("'size' must be an integer.")
 
+        self.size = size
         self.chromosomes = [
             OneMaxChromosome(length=chromosome_length) for _ in range(size)
         ]
